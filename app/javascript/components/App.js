a2a5e6a278b6f70945d4296c280dcc632e86e96f
Billy Downing <wldowning@gmail.com>
2019-03-07 09:22:09 +0100

    added react router to rails

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+// import GoogleMapReact from 'google-map-react';
+import Home from './Home'
+import Posts from './Posts'
+import NewPost from './NewPost'
+import Flat from './Flat';
+// import Marker from './Marker';
+// import './marker.css';
+// import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        Hello world!
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/posts" component={Posts} />
+          <Route exact path="/new_post" component={NewPost} />
+          <Route exact path="/flat" component={Flat} />
+        </Switch>
       </div>
     )
   }
